0bd625b5593b3d9457b2d2dc06970e4e25a5fe32
Mariano Arias <marianoarias123@hotmail.com>
2022-09-23 13:51:42 -0300

    dios soy yo de nuevo

--- src/components/firebase/firebase-config.js
+++ src/components/firebase/firebase-config.js
@@ -1,8 +1,8 @@
 export const firebaseConfig = {
-    apiKey: "AIzaSyBie1jSnEoX8_fyayp2BboVFohetck24vA",
-    authDomain: "nucba-4db7b.firebaseapp.com",
-    projectId: "nucba-4db7b",
-    storageBucket: "nucba-4db7b.appspot.com",
-    messagingSenderId: "1065632051551",
-    appId: "1:1065632051551:web:bd2d903fdb12991fb906ea"
-  };
+  apiKey: "AIzaSyDOn_qTZYqzBf4ag7kbGuOFyeIbnxa9890",
+  authDomain: "nucba-7789b.firebaseapp.com",
+  projectId: "nucba-7789b",
+  storageBucket: "nucba-7789b.appspot.com",
+  messagingSenderId: "366221756246",
+  appId: "1:366221756246:web:45ee69278c08b7314eb8f7"
+};
